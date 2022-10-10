8e89aadfa6e9dd8e971e731c8d154d0b5f0eb31b
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2022-10-10 18:53:15 +0530

    arraylist

--- ARRAY_LIST.java
+++ ARRAY_LIST.java
@@ -7,7 +7,10 @@ public class ARRAY_LIST {
         y1.add(10);
         y1.add(7);
         y1.add(34);
-        y1.add(0,1);
+        y1.add(0,1);// to add a  n element.
+        System.out.println(y1.contains(10)); //if the element is present or not.
+        System.out.println(y1.indexOf(10)); //takes the first index.
+        y1.set(1,14);
         for(int i=0;i< y1.size();i++){
             System.out.println(y1.get(i));
         }
